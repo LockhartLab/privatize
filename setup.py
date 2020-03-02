7849f81722f09d3548b9c3854619d2d3d00e86d6
doclockh <chris@lockhartlab.org>
2020-03-02 12:16:15 -0800

    Increased typelike min version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     install_requires=[
         'refunction>=0.0.2',
-        'typelike>=0.0.25',
+        'typelike>=0.0.28',
     ],
     python_requires='>=3.7',
     include_package_data=True,
